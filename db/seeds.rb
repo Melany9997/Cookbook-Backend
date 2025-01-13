191a5a9bedfb186a4399b13e5ab5cf4fbc9421e9
Melany Sierra <melanysabrina@icloud.com>
2025-01-13 12:43:48 +0000

    categories added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+Recipe.create(name: "Chocolate Ckake", description:"Delicious and chocolatey", image_url:"https://sugargeekshow.com/wp-content/uploads/2023/10/easy_chocolate_cake_slice.jpg", rating: 8.5)
+Recipe.create(name: "Lentil Bolognese", description: "Lovely vegetarian alternative", image_url:"https://www.twospoons.ca/wp-content/uploads/2020/05/vegan-lentil-bolognese-sauce-recipe-healthy-easy-twospoons-25-scaled.jpg", rating: 9.0)
+Recipe.create(name: "Sweet Potato soup", description: "An Autumnn favourite", image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD0Eb9a0ZGhmKvVPxlcJyBdIRGmnBV-tjSeA&s", rating: 9.5)
+Recipe.create(name: "Black bean Chilli", description:"A spicy and earthy delight", image_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSVQ73HYA3YGW9-5t-MUDsXyQUZUTN4jqTkA&s", rating: 8.0)
+Recipe.create(name: "Sweet and Tangy Tofu", description: "Delicious asian flavours", image_url:"https://vegancocotte.com/wp-content/uploads/2021/10/Sweet-and-Sour-Tofu-0.jpg", rating: 9.5)
