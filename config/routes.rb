187b4e72ddc6decd7dd5e4355e01d5b979a06412
Melany Sierra <melanysabrina@icloud.com>
2025-01-13 17:59:16 +0000

    all 8 steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :categories, except: [:edit, :update]
-  resources :bookmarks, only: [:delete]
+  resources :categories, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create,]
+  end
+
+  resources :bookmarks, only: [:destroy]
 
   #get "categories" to: "categories#index"
 
